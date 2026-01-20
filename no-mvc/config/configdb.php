4cff22061970a392b873282704cd0816cc5f87d8
Samuel-ssp <samuelsanchezponce.guadalupe@alumnado.fundacionloyola.net>
2026-01-20 18:21:26 +0100

    s

--- no-mvc/config/configdb.php
+++ no-mvc/config/configdb.php
@@ -1,13 +1,6 @@
 <?php
-<<<<<<< HEAD
-    define("SERVIDOR",'');
-    define("USUARIO",'');
-    define("PASSWORD",'');
-    define("BBDD",''); 
-=======
 define("SERVIDOR",'localhost');
 define("USUARIO",'root');
 define("PASSWORD",'');
 define("BBDD",'decklogy');
->>>>>>> parent of 188e3ac (Revert "Merge branch 'juntar-vistas'")
 ?>
--- php/config/configdb.php
+++ php/config/configdb.php
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-define("SERVIDOR",'');
-define("USUARIO",'');
-define("PASSWORD",'');
-define("BBDD",'');
-=======
+
 <?php
 define("SERVIDOR",'localhost');
 define("USUARIO",'root');
 define("PASSWORD",'');
 define("BBDD",'decklogy');
->>>>>>> parent of 188e3ac (Revert "Merge branch 'juntar-vistas'")
 ?>
